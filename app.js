e52ceec521b8c9fdf12a97f8ceb57587e0aeb83e
Kafi Anan Rafi <kafianan01@gmail.com>
2025-07-22 21:28:08 +0600

    add basic structure

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 import express from "express";
+import { configDotenv } from "dotenv";
+import cors from "cors";
+
+configDotenv();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Welcome to training management application" });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
